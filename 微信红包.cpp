ceb7cf9fefb83785ee7314070333626486691839
CeciliaWwq <1191408941@qq.com>
2019-12-01 20:29:35 +0800

    微信红包.cpp

---
+++ 微信红包.cpp
@@ -0,0 +1,17 @@
+/*
+class Gift {
+public:
+	int getValue(vector<int> gifts, int n) {
+		map<int, int>m;
+		for (int i = 0; i < n; i++)
+		{
+			++m[gifts[i]];
+			if (m[gifts[i]] >= n / 2)
+			{
+				return gifts[i];
+			}
+		}
+		return 0;
+	}
+};
+*/
