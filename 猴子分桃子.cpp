0a8d541cea99091e2bb37635e6c314f7f0af3899
CeciliaWwq <1191408941@qq.com>
2019-12-07 22:56:22 +0800

    奇数位上都是奇数或者偶数位上都是偶数.cpp 猴子分桃子.cpp

---
+++ 猴子分桃子.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	unsigned long n, a, b;
+	while (scanf("%lu", &n)) {
+		if (n == 0) break;
+
+		a = (unsigned long)pow(5, (double)n);
+		b = (unsigned long)pow(4, (double)n);
+
+		printf("%lu %lu\n", a - 4, b - 4 + n);
+	}
+	return 0;
+}
