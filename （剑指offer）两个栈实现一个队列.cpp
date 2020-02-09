89111bc6ac230a7f33ac2d548cff7ad9bf484799
CeciliaWwq <1191408941@qq.com>
2020-02-09 16:32:19 +0800

    （剑指offer)两个栈实现一个队列.cpp

---
+++ （剑指offer）两个栈实现一个队列.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+	void push(int node) {
+		stack1.push(node);
+	}
+
+	int pop() {
+		if (stack2.empty())
+		{
+			while (!stack1.empty())
+			{
+				int tmp = stack1.top();
+				stack1.pop();
+				stack2.push(tmp);
+			}
+			int a = stack2.top();
+			stack2.pop();
+			return a;
+		}
+		else {
+			int a = stack2.top();
+			stack2.pop();
+			return a;
+		}
+	}
+
+private:
+	stack<int> stack1;
+	stack<int> stack2;
+};
